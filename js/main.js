ec207c21ffca730aafb9af5aa184698c05af1b8f
Seham Anter Al amir <96702504+SehamAnter1@users.noreply.github.com>
2022-12-15 22:34:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var editorContainer = document.querySelector('.editorContainer1')
+CodeMirror(editorContainer, {
+  lineNumbers: true,
+  mode: 'javascript',
+  value: ''
+})
+var editorContainer = document.querySelector('.editorContainer2')
+CodeMirror(editorContainer, {
+  lineNumbers: true,
+  mode: 'javascript',
+  value: ''
+})
